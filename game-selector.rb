a2c71c0d433e1d47166a214f8cd3807f16c1443d
Brenna Flood <bren@brennx0r.com>
2019-11-09 11:38:45 -0800

    New: game selector and game list

---
+++ game-selector.rb
@@ -0,0 +1,23 @@
+filename = "game_list.txt"
+
+# read in the text file and save values as an array
+def arraycreate(filename)
+	array = IO.readlines(filename)
+	selector(array, filename)
+end
+
+# select the tsumtsum using sample method
+def selector(array, filename)
+	selected = array.sample
+	modifyfile(selected, filename)
+end
+
+# delete the selected item from the text file
+def modifyfile(selected, filename)
+	filetext = File.read(filename) 
+	new_contents = filetext.gsub(selected, "" )
+	File.open(filename, "w") { |file| file.puts new_contents }
+	puts selected.strip+" has been selected! Enjoy your game."
+end
+
+arraycreate(filename)
